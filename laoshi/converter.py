4eecf305937397478494c765edbd555e0c680fce
Ruben Serradas <rubenserradas@gmail.com>
2023-12-22 12:11:16 +0100

    Add linting and black

--- laoshi/converter.py
+++ laoshi/converter.py
@@ -1,24 +1,32 @@
+"""Module which provides the Converter class to convert characters"""
+from functools import reduce
 from opencc import OpenCC
 from pypinyin import pinyin
-from functools import reduce
 
 
 class Converter:
+    """Class for pinyin, traditional and simplified
+    chinese character conversions"""
 
     @staticmethod
     def to_traditional(hanzi: str) -> str:
-        return Converter.convert('s2t.json', hanzi)
+        """Transform simplified chinese characters to traditional"""
+        return Converter.convert("s2t.json", hanzi)
 
     @staticmethod
     def to_simplified(hanzi: str) -> str:
-        return Converter.convert('t2s.json', hanzi)
+        """Transform traditional chinese characters to simplified"""
+        return Converter.convert("t2s.json", hanzi)
 
     @staticmethod
     def to_pinyin(hanzi: str) -> str:
+        """Transform Chinese characters to pinyin.
+        We just take the first option from pypinyin."""
         flat_pinyin = [elem for py_sl in pinyin(hanzi) for elem in py_sl]
         return reduce(lambda a, b: a + b, flat_pinyin)
 
     @staticmethod
     def convert(ccdict: str, hanzi: str) -> str:
+        """Generic convertion method"""
         converter = OpenCC(ccdict)
         return converter.convert(hanzi)
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,18 +1,19 @@
+# [missing-module-docstring]
 import unittest
 from laoshi import converter
 
 
 class ConverterTest(unittest.TestCase):
-
+    # [missing-class-docstring]
     def setUp(self):
         pass
 
-    def test_to_traditional(self):
+    def test_to_traditional(self):  # [missing-function-docstring]
         self.assertEqual(converter.Converter.to_traditional("龙"), "龍")
 
-    def test_to_simplified(self):
+    def test_to_simplified(self):  # [missing-function-docstring]
         self.assertEqual(converter.Converter.to_simplified("龍"), "龙")
 
-    def test_to_pinyin(self):
+    def test_to_pinyin(self):  # [missing-function-docstring]
         self.assertEqual(converter.Converter.to_pinyin("你好"), "nǐhǎo")
         self.assertEqual(converter.Converter.to_pinyin("你"), "nǐ")
